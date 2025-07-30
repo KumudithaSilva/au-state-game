147f84c20c5b59071834d5ae57bf109592228bb0
Kumuditha <kumudithasilva66@gmail.com>
2025-07-30 17:41:58 +0530

    Add answer popup

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen.register_shape('au_statues.gif')
 turtle.shape('au_statues.gif')
 
 screen.setup(width=800, height=800)
+answer_state = screen.textinput(title="Guess the City", prompt="What's another city's name?")
+print(answer_state)
 
 def get_location_coordinates(x, y):
     print(x, y)
