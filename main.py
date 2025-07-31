e302f531c4452136f17455c1133f1fa64ff0cc3a
Kumuditha <kumudithasilva66@gmail.com>
2025-07-31 18:42:57 +0530

    missing city reminder file generation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import pandas
+import pandas as pd
 import csv
 import turtle
-from turtle import Screen, Turtle
+from turtle import Screen
 
 screen = Screen()
 screen.title("Australia State Game")
@@ -9,10 +9,11 @@ screen.register_shape('au_map.gif')
 turtle.shape('au_map.gif')
 screen.setup(width=810, height=810)
 
-data = pandas.read_csv("au_city.csv")
+data = pd.read_csv("au_city.csv")
 data_length = len(data)
 all_states = data.state.to_list()
 guessed_status = []
+missing_states = []
 
 def get_location_coordinates(x, y):
     au_state = screen.textinput(title="Save the City", prompt="Save the city's name")
@@ -20,24 +21,31 @@ def get_location_coordinates(x, y):
         writer = csv.writer(file)
         writer.writerow([au_state, x, y])
 
-def set_location_coordinates(x, y):
-    pass
-
-while len(guessed_status) < data_length:
-    answer_state = screen.textinput(title=f"{len(guessed_status)}/{data_length} Status Correct", prompt="What's another city's name?").title()
-    if answer_state in all_states:
-        guessed_status.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        state_data = data[data.state == answer_state]
-        x = state_data["x"].item()
-        y = state_data["y"].item()
-        t.goto(x, y)
-        t.dot(5, "red")
-        t.write(answer_state, font=("Impact", 15, "normal"))
-
+def set_location_coordinates(states):
+    t = turtle.Turtle()
+    t.hideturtle()
+    t.penup()
+    state_data = data[data.state == states]
+    x = state_data["x"].item()
+    y = state_data["y"].item()
+    t.goto(x, y)
+    t.dot(5, "red")
+    t.write(states, font=("Impact", 15, "normal"))
 
 # turtle.onscreenclick(set_location_coordinates)
+while len(guessed_status) < data_length:
+    answer_state = screen.textinput(title=f"{len(guessed_status)}/{data_length} Status Correct",
+                                    prompt="What's another city's name?").title()
+    if answer_state == "Exit":
+        missing_states = list(set(all_states) - set(guessed_status))
+        au_missing_statues = pd.DataFrame(missing_states, columns=["status"])
+        au_missing_statues.to_csv("au_statues_to_learn.csv", index=False)
+        break
+    if answer_state in all_states:
+        guessed_status.append(answer_state)
+        set_location_coordinates(answer_state)
+
+for state in missing_states:
+    set_location_coordinates(state)
+
 screen.exitonclick()
-turtle.mainloop()
